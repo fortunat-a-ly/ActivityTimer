6f8e3558cde5b8bfe77536d62047689c5b0a47d5
fortunat-a-ly <dubikw16@gmail.com>
2022-04-05 10:11:58 +0100

    add custom chronometer

---
+++ app/src/main/java/com/example/activitytimer/utils/timer/CustomChronometer.kt
@@ -0,0 +1,27 @@
+package com.example.activitytimer.utils.timer
+
+import android.os.SystemClock
+import android.widget.Chronometer
+
+class CustomChronometer(private val chronometer: Chronometer) {
+    var milliSeconds: Long = 0L
+
+    val base: Long get() = SystemClock.elapsedRealtime() - milliSeconds
+
+    val intervals = arrayListOf<Long>()
+
+    fun start() {
+        chronometer.start()
+    }
+
+    fun pause() {
+        milliSeconds = SystemClock.elapsedRealtime() - chronometer.base
+        chronometer.stop()
+    }
+
+    fun breakInterval() {
+        val newMilliseconds = SystemClock.elapsedRealtime() - chronometer.base
+        intervals.add(newMilliseconds - milliSeconds)
+        milliSeconds = newMilliseconds
+    }
+}
